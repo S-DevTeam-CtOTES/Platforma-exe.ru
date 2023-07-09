4afa065f1c6a777e3820c43e62cd64e9466e4882
Avdushin <avdushinbeaver1@gmail.com>
2023-07-10 01:08:38 +0300

    Updated TODOS

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // FIX: Fix aliases
   resolve: {
     alias: {
       '@': '/src',
@@ -13,5 +14,3 @@ export default defineConfig({
   },
   plugins: [react()],
 })
-
-// TODO: Aliases

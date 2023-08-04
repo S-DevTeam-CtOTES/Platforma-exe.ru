007f5c1e4e05e06db941cea05fd2091087fc8b6a
Avdushin <avdushinbeaver1@gmail.com>
2023-08-04 21:44:04 +0300

    Testing: Определение темы из ОС

--- Frontend/src/Features/toggle-theme/ui/ToggleTHeme.tsx
+++ Frontend/src/Features/toggle-theme/ui/ToggleTHeme.tsx
@@ -1,34 +1,38 @@
 import { useState, useEffect } from "react";
-
 import blueMoon from "@/Shared/assets/icons/blueMoon.svg";
 import blueSun from "@/Shared/assets/icons/blueSun.svg";
 import "./ToggleTheme.scss";
 
 const ToggleTheme = () => {
-  const [toggle, setToggle] = useState<boolean>(
-    () => localStorage.getItem("selectedTheme") === "dark"
+  const [theme, setTheme] = useState<string>(
+    () => localStorage.getItem("selectedTheme") || ""
   );
 
   useEffect(() => {
-    const currentTheme = localStorage.getItem("selectedTheme") || "light";
-    document.querySelector("body").setAttribute("data-theme", currentTheme);
-  }, []);
+    document.documentElement.setAttribute("data-theme", theme);
 
-  const setDarkMode = () => {
-    document.querySelector("body").setAttribute("data-theme", "dark");
-    localStorage.setItem("selectedTheme", "dark");
-    setToggle(true);
-  };
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
 
-  const setLightMode = () => {
-    document.querySelector("body").setAttribute("data-theme", "light");
-    localStorage.setItem("selectedTheme", "light");
-    setToggle(false);
-  };
+    // @Обработчик изменения темы ОС
+    const handleDarkModeChange = (event) => {
+      const newTheme = event.matches ? "dark" : "light";
+      setTheme(newTheme);
+      localStorage.setItem("selectedTheme", newTheme);
+    };
+
+    darkModeMediaQuery.addListener(handleDarkModeChange);
+
+    return () => {
+      darkModeMediaQuery.removeListener(handleDarkModeChange);
+    };
+  }, [theme]);
 
   const toggleTheme = () => {
-    if (toggle) setLightMode();
-    else setDarkMode();
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("selectedTheme", newTheme);
   };
 
   return (
@@ -37,13 +41,13 @@ const ToggleTheme = () => {
         type="checkbox"
         id="darkmode-toggle"
         className="theme__toggle-input"
-        checked={toggle}
+        checked={theme === "dark"}
         onChange={toggleTheme}
       />
       <span className="theme__toggle-slider theme__toggle-round">
         <div className="theme__toggle-slider-wrap ">
           <div className="theme__toggle-slider-wrap-img">
-            <img src={toggle ? blueMoon : blueSun} alt="toggle" />
+            <img src={theme === "dark" ? blueMoon : blueSun} alt="toggle" />
           </div>
         </div>
       </span>
